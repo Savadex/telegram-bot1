fcba9081b4cdd8eba0621a4d6ae9107573a33574
Savadex <efekansener2008@gmail.com>
2026-05-22 04:30:19 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,13 +1,12 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN = os.environ["8827021819:AAFImmcgaITO4cDayaSjeqMLP6RO1AQk5g0"]
+TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update, context):
     await update.message.reply_text("Bot aktif ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 
 print("Bot başladı...")
